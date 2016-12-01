c860cb1a7e9deb1f16f9b767bcba0b63bcd26cc8
jude <judejconroy@gmail.com>
2016-12-01 14:36:35 +0000

    updated mongo query

--- node/reportOne.js
+++ node/reportOne.js
@@ -1,27 +1,61 @@
 /**
  * Created by jude on 28/11/2016.
  */
+
 module.exports = {
 
     pieDuration: function (queryDate, venueid) {
 
+        var lessThan30 = 10;
+        var between3060 = 10;
+        var between6090 = 10;
+        var between90120 = 10;
+        var greaterThan120 = 10;
+        var previousMac = "";
+        var currentMac = "";
+        var currentTime = 0;
+        var previousTime = 0;
+
         var startDate = Date.parse(queryDate);
         var endDate = startDate + 1000 * 60 * 60 * 24;
 
-        //Customer visit duration stats.  Pie charts, yay!  <30 mins ; 30 – 60 mins ;
-        // 60 – 90 mins ; 90 – 120 mins ; > 120 mins  - average between selected dates
-
         //lets require/import the mongodb native drivers.
         var mongodb = require('mongodb');
-
-
         //We need to work with "MongoClient" interface in order to connect to a mongodb server.
         var MongoClient = mongodb.MongoClient;
-
         // Connection URL. This is where your mongodb server is running.
         var url = 'mongodb://localhost:27017/piData';
 
-        //this is a change
+        var calcFrequencies = function (data) {
+
+            currentMac = data.mac;
+            currentTime = data.captime;
+
+            if (previousMac !== currentMac)
+                previousTime = currentTime;
+
+            if (previousTime === 0) {
+                previousTime = currentTime;
+            }
+
+            if (currentTime - previousTime > 0 && currentTime - previousTime < 30 * 60 * 1000)
+                lessThan30++;
+
+            if (currentTime - previousTime > 30 * 60 * 1000 && currentTime - previousTime < 60 * 60 * 1000)
+                between3060++;
+
+            if (currentTime - previousTime > 60 * 60 * 1000 && currentTime - previousTime < 90 * 60 * 1000)
+                between6090++;
+
+            if (currentTime - previousTime > 90 * 60 * 1000 && currentTime - previousTime < 120 * 60 * 1000)
+                between90120++;
+
+            if (currentTime - previousTime > 120 * 60 * 1000)
+                greaterThan120++;
+
+            previousMac = data.mac;
+            previousTime = data.captime;
+        }
 
         // Use connect method to connect to the Server
         MongoClient.connect(url, function (err, db) {
@@ -29,48 +63,38 @@ module.exports = {
                 console.log('Unable to connect to the mongoDB server. Error:', err);
                 return;
             }
-                //HURRAY!! We are connected. :)
-                console.log('Connection established to', url);
+            //HURRAY!! We are connected. :)
+            console.log('Connection established to', url);
 
-                // Get the documents collection
-                var collection = db.collection('venueinfo');
+            // Get the documents collection
+            var collection = db.collection('venueinfo');
 
-                collection.aggregate([
-                    {
-                        $match: {
-                            venueid: { $eq: venueid},
-                            captime: { $gt: startDate, $lt: endDate}
-                        }
-                    },
-                    {
-                        $group: {
-                            _id: "$mac",
-                            count: {$sum: 1},
-                            30:    {$min :"$captime"},
-                            60:    {$max :"$captime"},
-                            90:    {$min :"$captime"},
-                            120:   {$max :"$captime"},
-                            120:   {$max :"$captime"}
-                        }
+            collection.find({
+                venueid: {$eq: venueid},
+                captime: {$gt: startDate, $lt: endDate}
+            }).toArray(function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else if (result.length) {
+                    console.log('Found:', result.length);
+                    for (var i = 0; i < result.length; i++) {
+                        calcFrequencies(result[i]);
                     }
-                ]
-                ).toArray(function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    } else if (result.length) {
-                        console.log('Found:', result);
-                    } else {
-                        console.log('No document(s) found with defined "find" criteria!');
-                    }
-                    //Close connection
-                    db.close();
-                });
+
+                } else {
+                    console.log('No document(s) found with defined "find" criteria!');
+                }
+            });
+            db.close;
         });
 
+
         var labels = ["< 30", "30 - 60", "60 - 90", "90 - 120", "> 120"];
-        var data = [250, 145, 99, 33, 5];
+        var data = [lessThan30, between3060, between6090, between90120, greaterThan120];
 
         return [labels, data];
     }
 }
+
+
 
