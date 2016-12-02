af605e3dd31fa98a16cf3089fd933b3b6a31ff86
jude <judejconroy@gmail.com>
2016-12-02 13:18:57 +0000

    updated mongo query

--- server.js
+++ server.js
@@ -24,8 +24,23 @@ app.get('/reportOne', function(request, response) {
 
 app.get('/serviceDay', function(request, response) {
 
-    var labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    var data = [[65, 59, 80, 81, 56, 55, 40], [28, 48, 40, 19, 86, 27, 90]];
+    var labels = [];
+    var data = [];
+
+    if(request.query.reportType === "7 Days") {
+        labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        data = [65, 59, 80, 81, 56, 55, 40];
+    }
+
+    if(request.query.reportType === "4 Weeks") {
+        labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
+        data = [65, 59, 80, 81];
+    }
+
+    if(request.query.reportType === "6 Months") {
+        labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+        data = [65, 59, 80, 81, 56, 55];
+    }
 
     response.send([labels, data]);
 
